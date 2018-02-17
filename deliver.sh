13b038eb4aaee325e8610d7f08ed918f0b488b98
Cyril Pham-le <cyril.phamle@gmail.com>
2018-02-17 14:31:02 +0100

    update

--- deliver.sh
+++ deliver.sh
@@ -7,4 +7,5 @@ cp docker-compose.yml $1/docker-compose.yml
 cp Dockerfile $1/Dockerfile
 cp update_docker_files.py $1/update_docker_files.py
 cp reverse-proxy-infos.json $1/reverse-proxy-infos.json
+cp -r data $1/data
 echo "End copying deployable files in app folder"
